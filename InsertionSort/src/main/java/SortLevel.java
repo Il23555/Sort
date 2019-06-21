f0fd44f136ce07d83b408953652d34970ed87e00
Ilyuza <ilyuza.01@mail.ru>
2019-06-21 10:43:32 +0300

    "create project"

---
+++ InsertionSort/src/main/java/SortLevel.java
@@ -0,0 +1,11 @@
+public class SortLevel {
+    import java.util.*;
+
+    public class SortLevel
+    {
+        public static T функция( параметры )
+        {
+            return ... ;
+        }
+    }
+}
