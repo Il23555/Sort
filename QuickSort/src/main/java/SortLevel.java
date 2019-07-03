d85122055e845633de2785c75871ec9b8c3cbf22
Ilyuza <ilyuza.01@mail.ru>
2019-07-03 14:30:49 +0300

    Add QuickSortOptimization

--- QuickSort/src/main/java/SortLevel.java
+++ QuickSort/src/main/java/SortLevel.java
@@ -18,6 +18,30 @@ public class SortLevel {
         }
     }
 
+    public static void QuickSortTailOptimization (int[] array, int left, int right) {
+        while (left < right) {
+            int pivot = array[(left + right) / 2];
+            int i = left;
+            int j = right;
+            while (i < j) {
+                while (array[i] < pivot)
+                    i++;
+                while (array[j] > pivot)
+                    j--;
+                if (i < j)
+                    swap(array, i, j);
+            }
+            if (i - left < right - i) {
+                QuickSortTailOptimization(array,left,i-1);
+                left = i + 1;
+            }
+            else {
+                QuickSortTailOptimization(array,i+1,right);
+                right = i - 1;
+            }
+        }
+    }
+
     public static void swap(int[] arr, int fromIdx, int toIdx) {
         int a = arr[fromIdx];
         arr[fromIdx] = arr[toIdx];
--- QuickSort/src/test/java/SortLevelTest.java
+++ QuickSort/src/test/java/SortLevelTest.java
@@ -1,13 +1,23 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SortLevelTest {
 
     @Test
     public void quickSort() {
-        int[] arr = new int[]{2,1,3,0,4,9,6,5};
+        int[] arr = new int[]{2,1,3,0,8,4,9,6,5,7};
         SortLevel.QuickSort(arr,0, arr.length-1);
         for (int i = 0; i < arr.length ; i++) {
-            System.out.print(arr[i]);
+            assertEquals(i,arr[i]);
+        }
+    }
+
+    @Test
+    public void quickSortTailOptimization() {
+        int[] arr = new int[]{2,1,3,0,8,4,9,6,5,7};
+        SortLevel.QuickSortTailOptimization(arr,0, arr.length-1);
+        for (int i = 0; i < arr.length ; i++) {
+            assertEquals(i,arr[i]);
         }
     }
 }
